9394770544b5f063dc259ff942b541812d5ef362
codeHawker <zhawken@gmail.com>
2022-04-26 19:50:37 +1200

    Updated app layout for mobile

--- src/components/NavLinks/NavLinks.js
+++ src/components/NavLinks/NavLinks.js
@@ -8,7 +8,6 @@ class NavLinks extends React.Component {
     return (
       <div className="topnav">
         <p className="page-title">TweetStat</p>
-        <a href="#">Contact</a>
         <a href="#" onClick={this.props.openAboutBox}>About</a>
         <a className="active" href="#home">Home</a>
       </div>
