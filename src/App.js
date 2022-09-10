8604810bdb59af0bd62a14126180238fc931dd3c
Zane <zhawken@gmail.com>
2022-09-11 11:49:34 +1200

    Make landing more intuitive

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      showAboutBox: false,
+      showAboutBox: true,
       seachState: 'empty'
     }
     this.handleSearch = this.handleSearch.bind(this)
--- src/components/AboutBox/AboutBox.js
+++ src/components/AboutBox/AboutBox.js
@@ -20,8 +20,11 @@ class AboutBox extends React.Component {
         <div className='about-box'>
           <div className='about-info'>
             <h2>TweetScan</h2>
-            <p>Twitter is a great way to stay up to date on the latest news, but it can be hard to keep track of everything. That's where our TweetScan comes in! We scrape tweets on your topic and provide you with valuable insight. This way, you can easily see what's being talked about and get a feel for the general sentiment.</p>
-            <button onClick={this.handleCloseButtonClick}>Close</button>
+            <p>Twitter is a great way to stay up to date on the latest news, but it can be hard to keep track of everything.</p>
+            <p>That's where our TweetScan comes in! We scrape tweets on your topic and provide you with valuable insight. </p>
+            <p>TweetScan shows you the most recent Tweets related to your search term, the most reTweeted, the most shared and also analyses the most frequently used terms</p>
+            <p>Click below to give it a go</p>
+            <button onClick={this.handleCloseButtonClick}>Get Started</button>
           </div>
 
         </div>
