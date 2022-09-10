c0824f4229265abd9dee3c2ca206cff2ae9a311e
Zane <zhawken@gmail.com>
2022-09-11 11:58:01 +1200

    Make landing more intuitive

--- src/components/AboutBox/AboutBox.js
+++ src/components/AboutBox/AboutBox.js
@@ -22,7 +22,7 @@ class AboutBox extends React.Component {
             <h2>TweetScan</h2>
             <p>Twitter is a great way to stay up to date on the latest news, but it can be hard to keep track of everything.</p>
             <p>That's where our TweetScan comes in! We scrape tweets on your topic and provide you with valuable insight. </p>
-            <p>TweetScan shows you the most recent Tweets related to your search term, the most reTweeted, the most shared and also analyses the most frequently used terms</p>
+            <p>TweetScan shows you the most recent Tweets related to your search term, the most reTweeted, the most liked and also analyses the most frequently used terms</p>
             <p>Click below to give it a go</p>
             <button onClick={this.handleCloseButtonClick}>Get Started</button>
           </div>
